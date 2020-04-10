fc6d6db13ea757e712adade756d1c63fc9b3d676
Patton Parham <patton.parham@verizon.net>
2020-04-09 19:27:47 -0500

    got custom soundcloud widget in js files ready to be imported

--- audio-generator/client/src/App.js
+++ audio-generator/client/src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./pages/main";
-import ReactWidget from "./components/ReactWidget/ReactWidget";
+// import ReactWidget from "./components/ReactWidget/ReactWidget";
 import GenerateBtn from "./components/generateButton/generateButton";
 import AddBtn from "./components/AddButton/AddButton";
-import SCWidget from "./components/SCWidget/SCWidget";
+// import SCWidget from "./components/SCWidget/SCWidget";
 import Header from "./components/header/header";
 import {Col, Row, Container } from "./components/Grid";
-
+// import SoundCloud from "./SoundCloud";
 const App = () => {
 
     const generateAudio = (event) => {
         event.preventDefault();
-        ReactWidget.play();
+
 
     }
 
@@ -25,7 +25,7 @@ const App = () => {
  <Container fluid>
      <Row>
          <Col size="lg">
-           <ReactWidget></ReactWidget>
+         
          </Col>
 
          
